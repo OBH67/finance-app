129aefe9b08034e1f050726bf71d3b16a1566d57
OBH67 <omarbalanzar113@gmail.com>
2021-07-12 23:45:38 -0700

    v1

---
+++ src/data/constant.js
@@ -0,0 +1,5 @@
+export const datos = [
+    {name: 'DEXTOOLS', info: 'Dex Tools Application', image: 'https://www.dextools.io/app/assets/img/logo/dextools_logo_header_large_dark_35.png', link: 'https://www.dextools.io/app/'},
+    {name: 'DXSALE', info: 'DXSale Network', image: 'https://dxsale.network/assets/media/dx-logo-200.png', link: 'https://dxsale.app/app/v2_9/defi-dashboard'},
+    {name: 'POOCOIN', info: 'Poo Coin BSC Charts', image: 'https://poocoin.app/images/logo/poocoin512.png', link: 'https://poocoin.app/'}
+  ]
