d480d6a37d3eea4a1db4f9817a54079000c3466f
OBH67 <omarbalanzar113@gmail.com>
2021-07-13 00:32:20 -0700

    v2

--- src/component/Home.js
+++ src/component/Home.js
@@ -5,13 +5,9 @@ import {Link} from 'react-router-dom'
 const Home = () => {
     return (
         <div class="bg-gradient-to-br from-indigo-900 to-green-900 min-h-screen overflow-auto">
-            <div class="container max-w-5xl mx-auto px-4">
-            <div class="w-4/5">
-                <h1 class="mt-32 text-white text-6xl font-bold">MY DEX'S</h1>
-            </div>
+            <div class="container max-w-5xl mx-auto px-4 my-32">
             <div class="text-white relative">
                 <div class="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-1 sm:gap-5 uppercase">
-
                 { datos.map((info) =>
                 <Link to={{ pathname: `${info.link}` }} target="_blank">
                 <div class="group flex items-center bg-indigo-900 bg-opacity-40 shadow-xl gap-5 px-6 py-5 rounded-lg ring-2 ring-offset-2 ring-offset-blue-800 ring-cyan-700 mt-5 cursor-pointer hover:bg-blue-900 hover:bg-opacity-100 transition">
