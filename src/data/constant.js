2f461b213fee9da3ee52ead6af3767aa810b85a2
OBH67 <omarbalanzar113@gmail.com>
2021-07-13 00:45:24 -0700

    v2

--- src/data/constant.js
+++ src/data/constant.js
@@ -1,8 +1,9 @@
 import image from '../images/logo.png'
 
 export const datos = [
-    {name: 'DEXTOOLS', info: 'Dex Tools Application', image: `${image}`, link: 'https://www.dextools.io/app/'},
+    {name: 'DEXTOOLS', info: 'Dex Tools Application', image: `${image}`, link: 'https://www.dextools.io/app/bsc'},
     {name: 'DXSALE', info: 'DXSale Network', image: 'https://dxsale.network/assets/media/dx-logo-200.png', link: 'https://dxsale.app/app/v2_9/defi-dashboard'},
     {name: 'POOCOIN', info: 'Poo Coin BSC Charts', image: 'https://poocoin.app/images/logo/poocoin512.png', link: 'https://poocoin.app/'},
-    {name: 'JUSTBUYITFFS', info: 'Token Research Assistan', image: 'https://apps.justbuyitffs.com/img/ffs_logo_sm.png', link: 'https://apps.justbuyitffs.com/?fbclid=IwAR0lVbPBjzcb6TQaSJa-dg183QXL8uoYRxPCtGfezER27q8qoWsVXamrj70'}
+    {name: 'JUSTBUYITFFS', info: 'Token Research Assistan', image: 'https://apps.justbuyitffs.com/img/ffs_logo_sm.png', link: 'https://apps.justbuyitffs.com/?fbclid=IwAR0lVbPBjzcb6TQaSJa-dg183QXL8uoYRxPCtGfezER27q8qoWsVXamrj70'},
+    {name: 'BscScan', info: 'Binance Smart Scanner' , image: 'https://images.ctfassets.net/i9fxydey74mt/1S36cQ2Ad4dSPohOBWahWk/4b67dfa21a186bc5385c443b66933d32/9eRdlbGE_400x400-removebg-preview-min.png?w=400&h=400&q=50&fm=png&fit=pad', link: 'https://bscscan.com/' }
   ]
